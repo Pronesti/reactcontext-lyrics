1b599c3901529781f35f4770d4fc9751ee3898c1
Diego Pronesti <dieh.diego@gmail.com>
2019-07-10 20:01:20 -0300

    finish second video

--- src/context.js
+++ src/context.js
@@ -12,7 +12,7 @@ export class Provider extends Component {
     componentDidMount() {
         axios.get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=us&f_has_lyrics=1&apikey=a7c365fd79b9c3ff7a81496c5493a4e1`)
         .then(res => {
-            console.log(res.data)
+            //console.log(res.data)
             this.setState({track_list: res.data.message.body.track_list})
         })
         .catch(err => console.log(err));
--- src/views/Lyrics.js
+++ src/views/Lyrics.js
@@ -1,5 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios'
+import Spinner from '../components/layout/Spinner';
+import {Link} from 'react-router-dom';
 
 const Lyrics = (props) => {
     const id = props.match.params.id;
@@ -22,18 +24,49 @@ const Lyrics = (props) => {
           const res = await axios.get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${id}&apikey=a7c365fd79b9c3ff7a81496c5493a4e1`)
           setLyric(res.data.message.body.lyrics);
           setLoading(false);
-          console.log(res.data.message.body.lyrics)
+         // console.log(res.data.message.body.lyrics)
         }
 
         fecthSong();
         fecthLyrics();
       }, [id]);
-    return (
-        <div>
-            <h1>{song.track_name}</h1> <h3>by {song.artist_name}</h3>
-            <p>{lyric.lyrics_body}</p>
-        </div>
-    )
-}
+
+      const checkGenre = () => {
+        var result = "not listed";
+        if (typeof(song.primary_genres) !== 'undefined'){
+          try{
+            result = song.primary_genres.music_genre_list[0].music_genre.music_genre_name_extended; 
+          }catch(err){
+            //console.log(err)
+          }
+         
+        }
+        return result;
+      }
+
+        const Loaded = (<div className="card">
+          <h5 className="card-header"><strong>{song.track_name}</strong> <span className="text-secondary">by {song.artist_name}</span></h5>
+          <div className="card-body"><div className="card-text"><p>{lyric.lyrics_body}</p></div></div>
+          <ul className="list-group mt-3">
+          <li className="list-group-item">
+          <strong>Album</strong>: {song.album_name}
+          </li>
+          <li className="list-group-item">
+          <strong>Genre</strong>: {checkGenre()}
+          </li>
+          <li className="list-group-item">
+          <strong>Rating</strong>: {song.track_rating}
+          </li>
+          </ul>
+      </div>);
+      
+      return (
+        <React.Fragment>
+        <Link to="/" className="btn btn-dark btn-sm mb-4 ml-4"> Go back</Link>
+        {loading ? <Spinner /> : Loaded }
+        </React.Fragment>
+      );
+      }
+   
 
 export default Lyrics
